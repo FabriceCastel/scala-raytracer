da1c293e3ed2c950ea53cc33d264f8a0152adc85
Fabrice Castel <fabrice.castel@hotmail.com>
2015-02-21 16:04:57 -0500

    added some skeleton code for parser/scenenode/renderparams

---
+++ src/main/scala/Parser.scala
@@ -0,0 +1,19 @@
+package raytracer.Parser
+
+import raytracer.SceneNode._
+import raytracer.RenderParameters._
+
+// use this class to parse whichever scripting language into
+// a tree of SceneNodes and return its root && render params
+
+class Parser(filename : String){
+	def getScene() : SceneNode = {
+		val scene = new SceneNode("super magical node")
+		return scene
+	}
+
+	def getRenderParameters() : RenderParameters = {
+		val params = new RenderParameters(600, 600)
+		return params
+	}
+}
