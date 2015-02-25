750d3c3397cb80b198f6fa2b556c10fe18ab5c39
Fabrice Castel <fabrice.castel@hotmail.com>
2015-02-24 22:29:57 -0500

    merge confs

--- src/main/scala/Ray.scala
+++ src/main/scala/Ray.scala
@@ -3,6 +3,6 @@ package raytracer.Ray
 import javax.vecmath.{Point3d}
 import javax.vecmath.{Vector3d}
 
-class Ray(val origin: Point3d, val direction: Vector3d){
-	require(direction.length != 0, "Cannot instantiate a Ray object with the zero vector")
+class Ray(val p: Point3d, val v: Vector3d){
+	require(v.length != 0, "Cannot instantiate a Ray object with the zero vector")
 }
