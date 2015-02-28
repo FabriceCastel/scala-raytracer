b99417392381838df7a09d8c485c77e8a2caf937
Fabrice Castel <fabrice.castel@hotmail.com>
2015-02-28 14:28:09 -0500

    added testing functionality, Triangle class and vecmath wrapper tweaks

--- build.sbt
+++ build.sbt
@@ -1,1 +1,5 @@
-libraryDependencies += "org.scala-lang" % "scala-swing" % "2.10.0"
+scalaVersion := "2.11.4"
+
+libraryDependencies += "org.scala-lang.modules" %% "scala-swing" % "1.0.1"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
--- src/main/scala/BasicIntersection.scala
+++ src/main/scala/BasicIntersection.scala
@@ -4,15 +4,15 @@ import raytracer.vecmath._
 
 // Intersection returned by Primitive subclasses
 
-class BasicIntersection(val point: Point3D, val vector: Vector3D, val t: Double, val uv: Point2D, val isValid: Boolean){
-	def this(point: Point3D, vector: Vector3D, t: Double, uv: Point2D){
-		this(point, vector, t, uv, true)
+class BasicIntersection(val point: Point3D, val normal: Vector3D, val t: Double, val uv: Point2D, val isValid: Boolean){
+	def this(point: Point3D, normal: Vector3D, t: Double, uv: Point2D){
+		this(point, normal, t, uv, true)
 		require(uv.x >= 0 && uv.y >= 0 && uv.x <= 1 && uv.y <= 1, "UV lies outside of expected range")
-		require(vector.length != 0, "Cannot instantiate a BasicIntersection object with the zero vector")
+		require(normal.length != 0, "Cannot instantiate a BasicIntersection object with the zero vector")
 	}
 
-	def this(point: Point3D, vector: Vector3D, t: Double){
-		this(point, vector, t, new Point2D(0,0))
+	def this(point: Point3D, normal: Vector3D, t: Double){
+		this(point, normal, t, new Point2D(0,0))
 	}
 
 	def this(){
