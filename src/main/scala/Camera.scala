6be3a71894994b95c280aea26d34f48d68205147
Fabrice Castel <fabrice.castel@hotmail.com>
2015-03-01 18:36:16 -0500

    fixed cam ray gen, added test anim cycle in Raytracer.scala

--- src/main/scala/Camera.scala
+++ src/main/scala/Camera.scala
@@ -10,30 +10,28 @@ class Camera(position: Point3D, direction: Vector3D, up: Vector3D, fov: Double){
 		this(new Point3D(0,0,-100), new Vector3D(0,0,1), new Vector3D(0,1,0), 50)
 	}
 
-	// ********** TODO *********
-	//
-	// direct port of the old INCOMPLETE C++ code
-	// this doesn't take any of the direction or up vectors
-	// into consideration and does a hack-ass job
-	//
 
 	def generateRays(width: Int, height: Int): Array[Array[Ray]] = {
+		// Adapted from
+		// http://stackoverflow.com/questions/14094042/calculating-camera-ray-direction-to-3d-world-pixel
 		val fovx = Math.PI * fov / 360.0
 		val fovy = fovx * height / width
 
 		var rayGrid = Array.ofDim[Ray](width, height)
 
-		for(x <- 0 to width-1; y <- 0 to height-1){
-			val rdir = new Vector3D(
-				(2.0*x - width)/width * tan(fovx),
-				(-1 * (2.0*y - height)/height) * tan(fovy),
-				1)
-			rayGrid(x)(y) = new Ray(position, rdir)
+		var nright = up cross direction; nright.normalize
+		var nup = nright cross direction; nup.normalize
+		var dir = direction; dir.normalize
+
+		val ulen = tan(fovx)
+		val vlen = tan(fovy)
+
+		for(x <- 0 to width - 1; y <- 0 to height - 1){
+			val adjx = (2.0*x - width) / width
+			val adjy = (2.0*y - height) / height
+			val pixel = position + nright*adjx*ulen + nup*adjy*vlen + dir
+			rayGrid(x)(y) = new Ray(position, pixel - position)
 		}
 		rayGrid
 	}
-
-	//
-	// ********* /TODO *******
-	//
 }
