4813ec8731d7cdbd8d110318baf471183eb4bcab
Fabrice Castel <fabrice.castel@hotmail.com>
2015-03-08 13:27:50 -0400

    revert to old style for efficiency

--- src/main/scala/primitive/Triangle.scala
+++ src/main/scala/primitive/Triangle.scala
@@ -17,33 +17,38 @@ class Triangle(vertices: List[Point3D], uv: List[Point2D], hasUV: Boolean) exten
 	}
 
 	override def intersect(ray: Ray): BasicIntersection = {
-		val intersection: Option[BasicIntersection] = for{
-			edge1 <- (vertices(1) - vertices(0))
-			edge2 <- (vertices(2) - vertices(0))
-			P = ray.v cross edge2
-			determinant = edge1 dot P
-			if !(determinant > -Utils.EPSILON && determinant < Utils.EPSILON) // fail
-			inv_determinant = 1.0/determinant
-			T = ray.p - vertices(0)
-			u = (T dot P) * inv_determinant
-			if !(u < 0 || u > 1) // fail
-			Q = T cross edge1
-			v = (ray.v dot Q) * inv_determinant
-			if !(v < 0 || u + v > 1) // fail
-			t = (edge2 dot Q) * inv_determinant
-			if !(t < Utils.EPSILON) // fail
-			hit = ray.p + ray.v*t
-		} yield {
-			if(hasUV){
-				val d0 = Math.abs((hit - vertices(0)).length)
-				val d1 = Math.abs((hit - vertices(1)).length)
-				val d2 = Math.abs((hit - vertices(2)).length)
-				val uvAvg = (uv(0)*d0 + uv(1)*d1 + uv(2)*d2) / (d0 + d1 + d2)
-				new BasicIntersection(hit, edge1 cross edge2, t, uvAvg)
-			} else {
-				new BasicIntersection(hit, edge1 cross edge2, t)
+		val fail = new BasicIntersection()
+		val edge1 = vertices(1) - vertices(0)
+		val edge2 = vertices(2) - vertices(0)
+		val P = ray.v cross edge2
+		val determinant = edge1 dot P
+		if(determinant > -Utils.EPSILON && determinant < Utils.EPSILON) fail
+		else {
+			val inv_determinant = 1.0/determinant
+			val T = ray.p - vertices(0)
+			val u = (T dot P) * inv_determinant
+			if(u < 0 || u > 1) fail
+			else {
+				val Q = T cross edge1
+				val v = (ray.v dot Q) * inv_determinant
+				if(v < 0 || u + v > 1) fail
+				else {
+					val t = (edge2 dot Q) * inv_determinant
+					if(t < Utils.EPSILON) fail
+					else {
+						val hit = ray.p + ray.v*t;
+						if(hasUV){
+							val d0 = Math.abs((hit - vertices(0)).length)
+							val d1 = Math.abs((hit - vertices(1)).length)
+							val d2 = Math.abs((hit - vertices(2)).length)
+							val uvAvg = (uv(0)*d0 + uv(1)*d1 + uv(2)*d2) / (d0 + d1 + d2)
+							new BasicIntersection(hit, edge1 cross edge2, t, uvAvg)
+						} else {
+							new BasicIntersection(hit, edge1 cross edge2, t)
+						}
+					}
+				}
 			}
 		}
-		intersection.getOrElse(new BasicIntersection())
 	}
 }
